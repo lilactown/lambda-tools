2ad13c4e4e6c7715684904d9a7461970ca8a6cb3
Will Acton <will@willacton.com>
2018-01-08 09:17:31 -0800

    Core

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # lambda-tools
 
-A Clojure library designed to provide a sane API for building serverless Clojure in AWS Lambda
+Tools for building serverless Clojure applications in AWS Lambda
 
 ## Usage
 
-FIXME
+```clojure
+(ns demoaws.core
+  (:require [demoaws.lambda :refer [gen-lambda]]
+            [demoaws.middleware :refer [ring-adapter]]))
+
+;; Use a run-of-the-mill Ring handler
+(defn app [request]
+  {:status 200
+   :headers {}
+   :body "<h1>Hello, world</h1>"})
+
+(def handler
+  (-> app
+      ;; Apply the HTTP Ring adapter middleware
+      ;; to convert the AWS HTTP event to a Ring-
+      ;; compatible request
+      ring-adapter))
+
+(gen-lambda
+  {:name demoaws.core.Greet
+   :handler handler})
+```
 
 ## License
 
--- src/lambda_tools/core.clj
+++ src/lambda_tools/core.clj
@@ -1,6 +1,31 @@
-(ns lambda-tools.core)
+(ns lambda-tools.core
+  (:require [clojure.data.json :as json]
+            [clojure.string :as s]
+            [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn stream->edn [in]
+  (json/read (io/reader in)))
+
+(defn edn->stream [edn out]
+  (let [w (io/writer out)]
+    (json/write edn w)
+    (.flush w)))
+
+(defmacro gen-lambda
+  "Create a named class that can be invoked as an AWS Lambda function."
+  [{:keys [name handler simple]}]
+  (let [prefix (gensym)
+        handleRequestMethod (symbol (str prefix "handleRequest"))]
+    `(do
+       (gen-class
+        :name ~name
+        :prefix ~prefix
+        :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+       ~(if (false? simple)
+          `(def ~handleRequestMethod ~handler)
+          `(defn ~handleRequestMethod
+             [this# in# out# ctx#]
+             (-> (stream->edn in#)
+                 (~handler ctx#)
+                 (edn->stream out#)))))))
